0a152c4d9538d48c7aa63b568ed606d3210d3dc0
WeijiangChen <ourweijiang@gmail.com>
2024-05-10 12:07:42 +0800

    增加微信硬件设备凭证接口参数

--- wechatpy/iot/client/api/device.py
+++ wechatpy/iot/client/api/device.py
@@ -5,16 +5,17 @@ from wechatpy.client.api.base import BaseWeChatAPI
 
 
 class IotDevice(BaseWeChatAPI):
-    def get_device_ticket(self, ilink_im_sdk_id, user_openid):
+    def get_device_ticket(self, ilink_im_sdk_id, user_openid, ticket_scene=0):
         """
         获取设备ticket
         https://iot.weixin.qq.com/doc?page=3-3
 
         :param ilink_im_sdk_id: 设备sdk id
         :param user_openid: 用户openid
+        :param ticket_scene: 0:私人模式绑定场景. 4:公共模式绑定场景. 不填默认为0
         :return: ilink_device_ticket
         """
-        data = {"ilink_im_sdk_id": ilink_im_sdk_id, "user_openid": user_openid}
+        data = {"ilink_im_sdk_id": ilink_im_sdk_id, "user_openid": user_openid, "ticket_scene": ticket_scene}
         result = self._post("get_device_ticket", json=data)
         return result
 
