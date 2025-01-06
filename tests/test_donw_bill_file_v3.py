b8347ac57ac7bf675e64ff0d8b682ae8f4cfbc1f
tanwei <tanwei@liankenet.com>
2025-01-06 09:19:23 +0800

    下载账单api mock测试完成

--- tests/test_donw_bill_file_v3.py
+++ tests/test_donw_bill_file_v3.py
@@ -14,6 +14,20 @@ _FIXTURE_PATH = os.path.join(_TESTS_PATH, "fixtures", "pay/v3/")
 
 
 @urlmatch(netloc=r"(.*\.)?api\.mch\.weixin\.qq\.com$")
+def wechat_api_down_file_mock(url, request):
+    res_file = os.path.join(_FIXTURE_PATH, "zd.xlsx")
+    headers = {
+        'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',  # 指定内容类型为纯文本
+        'Content-Disposition': 'attachment; filename=zd.xlsx'  # 可选，提示浏览器以文件下载的方式处理响应
+    }
+    try:
+        f = open(res_file, "rb")
+        f1 = f.read()
+    except (IOError, ValueError):
+        pass
+    return response(200, f1, headers, request=request)
+
+@urlmatch(netloc=r"(.*\.)?api\.mch\.weixin\.qq\.com$")
 def wechat_api_mock(url, request):
     path = (url.path[1:] if url.path.startswith("/") else url.path).replace("v3/", "").replace("/", "_")
     res_file = os.path.join(_FIXTURE_PATH, f"{path}.json")
@@ -32,16 +46,56 @@ def wechat_api_mock(url, request):
 class DownBillFileTestCase(unittest.TestCase):
     def setUp(self):
         self.client = WeChatPay(
-            appid="abc1234",
-            apiv3_key="test123",
-            mch_id="1192221",
+            appid="11",
+            apiv3_key="",
+            mch_id="11",
             wechat_cert_dir=_CERTS_PATH,
             apiclient_cert_path=os.path.join(_CERTS_PATH, "apiclient_cert.pem"),
             apiclient_key_path=os.path.join(_CERTS_PATH, "apiclient_key.pem"),
             skip_check_signature=True,  # 测试无法校验证书
         )
 
-    def test_media(self):
+    def test_trade_bill(self):
         with HTTMock(wechat_api_mock):
             response = self.client.ecommerce.trade_bill("2024-12-31")
-            print(response)
+            self.assertIn("hash_type", response)
+            self.assertIn("hash_value", response)
+            self.assertIn("download_url", response)
+
+    def test_fund_flow_bill(self):
+        with HTTMock(wechat_api_mock):
+            response = self.client.ecommerce.fund_flow_bill("2024-12-31")
+            self.assertIn("hash_type", response)
+            self.assertIn("hash_value", response)
+            self.assertIn("download_url", response)
+
+
+    def test_profit_sharing_bill(self):
+        with HTTMock(wechat_api_mock):
+            response = self.client.ecommerce.profit_sharing_bill("2024-12-31")
+            self.assertIn("hash_type", response)
+            self.assertIn("hash_value", response)
+            self.assertIn("download_url", response)
+
+    def test_eco_fund_flow_bill(self):
+        with HTTMock(wechat_api_mock):
+            response = self.client.ecommerce.eco_fund_flow_bill("2024-12-31")
+            self.assertIn("download_bill_count", response)
+            self.assertIn("download_bill_list", response)
+
+    def test_sub_mch_fund_flow_bill(self):
+        with HTTMock(wechat_api_mock):
+            response = self.client.ecommerce.sub_mch_fund_flow_bill(1657489417,"2024-12-31",'BASIC')
+            self.assertIn("download_bill_count", response)
+            self.assertIn("download_bill_list", response)
+
+
+    def test_download_bill(self):
+        with HTTMock(wechat_api_down_file_mock):
+            con = self.client.ecommerce.download_bill('https://api.mch.weixin.qq.com/v3/billdownload/file')
+            target_file_path = os.path.join(_FIXTURE_PATH, "zd1.xlsx")
+            try:
+                with open(target_file_path, 'wb') as target_file:
+                    target_file.write(con)
+            except Exception as e:
+                print(e)
